c88bef6b923d71e8b22f46c89e0d173a52628870
Mateusz <mateusz.harlos@gmail.com>
2020-11-18 15:29:56 +0000

    cinema lab

---
+++ models/cinema.js
@@ -0,0 +1,38 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+module.exports = Cinema;
+
+Cinema.prototype.filmTitles = function () {
+  const filmTitles = this.films.map(film => film.title);
+  return filmTitles;
+};
+
+Cinema.prototype.findTitle = function (title) {
+  const findTitle = this.films.find(film => film.title === title);
+  return findTitle;
+};
+
+Cinema.prototype.filmsByProperty = function (property, value) {
+  const filterByProperty = this.films.filter(film => film[property] === value);
+  console.log(filterByProperty);
+  return filterByProperty;
+};
+
+Cinema.prototype.checkIfMadeInYear = function (year) {
+  const checkYear = this.films.some(film => film.year === year);
+  return checkYear;
+};
+
+Cinema.prototype.checkIfAll = function (length) {
+  const checkIfAll = this.films.every(film => film.length > length);
+  return checkIfAll;
+};
+
+Cinema.prototype.totalRunningTime = function () {
+  const totalRunningTime = this.films.reduce((acc, value) => {
+    return acc + value.length
+  },0);
+  return totalRunningTime;
+};
